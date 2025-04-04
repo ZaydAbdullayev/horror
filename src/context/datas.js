78689b856932606b4bfe77381a5ea1d23e37af92
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-04-04 17:07:28 +0500

    update

--- src/context/datas.js
+++ src/context/datas.js
@@ -1,5 +1,6 @@
 import example1 from '../assets/example-1.jpg'
 import example2 from '../assets/example-2.jpg'
+import example3 from '../assets/example-3.jpg'
 
 export const examples = [
     { videoId: 'Rs3t326SyY4', type: 'video', img: '' },
@@ -8,5 +9,5 @@ export const examples = [
     { videoId: '', type: 'img', img: example2 },
     { videoId: 'wW70p9o64_o', type: 'video', img: '' },
     { videoId: 'o3LqFpCCn0g', type: 'video', img: '' },
-    { videoId: 'vhNLkZyRYkA', type: 'video', img: '' },
+    { videoId: '', type: 'img', img: example3 },
 ]
